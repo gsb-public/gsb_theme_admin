8b0a7bf646dc06447f484006979e52f09f5ee036
Alexei Gorobets <asgorobets@gmail.com>
2013-06-25 18:56:24 +0300

    WPBQA-143: Remove file iconsfrom markup.

--- template.php
+++ template.php
@@ -1,5 +1,14 @@
 <?php
 
 function gsb_theme_admin_form_panelizer_edit_content_form_alter(&$form, &$form_state, $form_id) {
-    $form['display']['#markup'] = $form['display']['#markup'] . '<div class="clearfix"></div>';
-  }
+  $form['display']['#markup'] = $form['display']['#markup'] . '<div class="clearfix"></div>';
+}
+
+/**
+ * Overrides theme_file_icon().
+ *
+ * Remove file icon.
+ */
+function gsb_theme_admin_file_icon($variables) {
+  return '';
+}

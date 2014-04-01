3486db52fa1df2e2a7df6d611d635e1867c00e6b
Tim Plunkett <git@plnktt.com>
2014-04-01 12:44:15 -0700

    Allow submit buttons to bypass form dataloss prevention.

--- js/editing_message.js
+++ js/editing_message.js
@@ -2,10 +2,16 @@
 
   Drupal.behaviors.editingMessage = {
     attach: function () {
-      $('form.node-form').FormObserve({
+      var $form = $('form.node-form');
+      $form.FormObserve({
         changeClass: false,
         msg: Drupal.t("You've made changes on one or more tabs. Click 'Save and Publish' to save all changes!")
       });
+
+      // Allow submit buttons to bypass the message.
+      $form.submit(function () {
+        $(this).FormObserve_save();
+      })
     }
   };
 

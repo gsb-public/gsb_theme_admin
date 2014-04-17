948cb27e54a906ee4996ef1fc05bf3ccd3d4d038
Alexei Gorobets <asgorobets@gmail.com>
2014-04-17 13:48:32 +0300

    WP-3456: Hide 'Show row weights' link for faculty.

--- template.php
+++ template.php
@@ -1,5 +1,13 @@
 <?php
 
+function gsb_theme_admin_preprocess_html(&$vars) {
+  global $user;
+  if (in_array('Faculty', $user->roles)) {
+    $vars['classes_array'][] = 'faculty-role';
+  }
+
+}
+
 function gsb_theme_admin_form_panelizer_edit_content_form_alter(&$form, &$form_state, $form_id) {
   $form['display']['#markup'] = $form['display']['#markup'] . '<div class="clearfix"></div>';
 }

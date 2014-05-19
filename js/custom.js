9163b36350df77af8e9cba7366b64c85860d44f0
Tim Plunkett <git@plnktt.com>
2014-05-19 12:54:22 -0700

    WP-3620: Auto-upload files.

--- js/custom.js
+++ js/custom.js
@@ -19,4 +19,16 @@
       });
     }
   };
+
+  Drupal.behaviors.fileAutoUpload = {
+    attach: function (context) {
+      $(context).find('input[type="file"]').once('auto-file-upload').change(function (event) {
+        $(event.target)
+          .closest('.form-managed-file')
+          .find('.form-submit')
+          .trigger('mousedown');
+      });
+    }
+  };
+
 }(jQuery));
